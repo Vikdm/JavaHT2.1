5f9b28d05aea0ad43b397cba4339a198f4f7d69e
Vikdm <viktorov@my.com>
2022-04-24 20:38:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+       int amount = 1204;
+       int freeMiles = 20;
+       int bonus = amount / freeMiles;
+
+        System.out.println("Бесплатные мили:" + bonus);
+
+    }
+
+}
